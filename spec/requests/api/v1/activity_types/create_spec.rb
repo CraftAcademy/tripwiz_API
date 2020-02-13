f843813c78a13df58d32a4e5b105a46fb92aede4
mantanquzzmo <quzzmo@hotmail.com>
2020-02-13 13:31:17 +0100

    edits create_activity_type to accept extra param

--- spec/requests/api/v1/activity_types/create_spec.rb
+++ spec/requests/api/v1/activity_types/create_spec.rb
@@ -78,4 +78,27 @@ RSpec.describe 'POST /api/v1/activity_type', type: :request do
       expect(response_json['error']).to eq 'Failed to create activity.'
     end
   end
+
+  describe 'Successfully creates more restaurants' do
+    let!(:activity_type) { create(:activity_type, activity_type: "restaurant", trip_id: trip.id) }
+    let!(:activity) { create_list(:activity, 3, activity_type_id: activity_type.id) }
+    before do
+      get_google_places_restaurants_success
+
+      post '/api/v1/activity_types',
+           params: { activity_type: 'restaurant',
+                     keyword: 'chinese',
+                     trip: trip.id,
+                     max_price: '2',
+                     additional_activity: "yes"  }
+    end
+
+    it 'returns a 200 response status' do
+      expect(response).to have_http_status 200
+    end
+
+    it 'returns 4 restaurants' do
+      expect(response_json.length).to eq 4
+    end
+  end
 end
