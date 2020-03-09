cce84d3e13d47a3682068328831d64cc47a123a4
mantanquzzmo <quzzmo@hotmail.com>
2020-03-09 11:19:24 -0400

    code formatting

--- app/controllers/api/v1/ratings_controller.rb
+++ app/controllers/api/v1/ratings_controller.rb
@@ -13,15 +13,15 @@ class Api::V1::RatingsController < ApplicationController
       end
     end
 
-    rating = Rating.create(trip_id: params[:trip], user_id: current_user.id,
-                           destination_rating: params[:destination_rating],
-                           activities_rating: params[:activities_rating],
-                           restaurants_rating: params[:restaurants_rating],
-                           hotel_rating: params[:hotel_rating])
-    if rating.persisted?
-      render json: rating, status: 200
+    ratings = Rating.create(trip_id: params[:trip], user_id: current_user.id,
+                            destination_rating: params[:destination_rating],
+                            activities_rating: params[:activities_rating],
+                            restaurants_rating: params[:restaurants_rating],
+                            hotel_rating: params[:hotel_rating])
+    if ratings.persisted?
+      render json: ratings, status: 200
     else
-      render json: { error: rating.errors.full_messages }, status: 422
+      render json: { error: ratings.errors.full_messages }, status: 422
     end
   end
 
@@ -33,7 +33,14 @@ class Api::V1::RatingsController < ApplicationController
 
   def update
     :authenticate_user!
+
     rating_to_update = Rating.where(trip_id: params[:id])
+
+    if rating_to_update[0].user_id != current_user.id
+      render json: { error: 'Only authorized to update own ratings' }, status: 401
+      return
+    end
+
     rating_to_update.update(update_params)
 
     render json: rating_to_update[0], status: 200
@@ -42,6 +49,6 @@ class Api::V1::RatingsController < ApplicationController
   private
 
   def update_params
-    params.permit(:destination_rating, :activities_rating, :restaurants_rating, :hotel_rating).select { |k, v| !v.nil? }
+    params.permit(:destination_rating, :activities_rating, :restaurants_rating, :hotel_rating).reject { |_k, v| v.nil? }
   end
 end
--- spec/requests/api/v1/ratings/update_spec.rb
+++ spec/requests/api/v1/ratings/update_spec.rb
@@ -1,9 +1,14 @@
 # frozen_string_literal: true
 
 RSpec.describe 'GET /api/v1/trips/:id', type: :request do
-  let(:trip) { create(:trip) }
   let(:user) { create(:user) }
+  let(:trip) { create(:trip) }
   let!(:rating) { create(:rating, trip_id: trip.id, user_id: user.id) }
+  let(:credentials) { user.create_new_auth_token }
+  let!(:headers) { { HTTP_ACCEPT: 'application/json' }.merge!(credentials) }
+  let(:user2) { create(:user) }
+  let(:credentials2) { user2.create_new_auth_token }
+  let!(:headers2) { { HTTP_ACCEPT: 'application/json' }.merge!(credentials2) }
 
   describe 'Succesfully updates ratings' do
     before do
@@ -41,6 +46,18 @@ RSpec.describe 'GET /api/v1/trips/:id', type: :request do
 
     it 'returns updated ratings' do
       expect(response_json['destination_rating']).to eq 4
+    end
+  end
+
+  describe 'Can only update its own ratings' do
+    before do
+      put "/api/v1/ratings/#{trip.id}",
+          params: { trip: trip.id,
+                    hotel_rating: 3 }, headers: headers2
+    end
+
+    it 'returns a 401 response status' do
+      expect(response).to have_http_status 401
     end
   end
 end
