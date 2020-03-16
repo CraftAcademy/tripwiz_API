d22318e98b5261be8aa4368d29f8cbbcefe6b33c
mantanquzzmo <quzzmo@hotmail.com>
2020-03-16 19:40:46 -0400

    adds further testing

--- spec/requests/api/v1/ratings/create_spec.rb
+++ spec/requests/api/v1/ratings/create_spec.rb
@@ -43,4 +43,18 @@ RSpec.describe 'POST /api/v1/trips', type: :request do
       expect(response_json['error']).to eq 'Trip already rated, please update'
     end
   end
+
+  describe 'Succesfully rating part of trip' do
+    before do
+      post '/api/v1/ratings',
+           params: { trip: trip.id,
+                     destination_rating: 4,
+                     restaurants_rating: 4,
+                     hotel_rating: 4 }, headers: headers
+    end
+
+    it 'returns a 200 response status' do
+      expect(response).to have_http_status 200
+    end
+  end
 end
